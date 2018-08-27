c0a63e30dc5e791613f824d8f030d1a6521d0c0f
Davy Meireles <davy.meireles885@gmail.com>
2018-08-27 12:53:29 -0300

    ajustes no endpoint/categorias/id

--- src/main/java/com/davym/cursomc/services/CategoriaService.java
+++ src/main/java/com/davym/cursomc/services/CategoriaService.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Service;
 
 import com.davym.cursomc.domain.Categoria;
 import com.davym.cursomc.repositories.CategoriaRepository;
+import com.davym.cursomc.services.exceptions.ObjectNotFoundException;
 
 @Service
 public class CategoriaService {
@@ -14,8 +15,11 @@ public class CategoriaService {
 	@Autowired
 	private CategoriaRepository repo;
 	
-	public Categoria buscar(Integer id) {
+	public Categoria buscar(Integer id) 
+	{
 		Optional<Categoria> obj = repo.findById(id);
-		return obj.orElse(null);
-		}
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+		"Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName()));
+	}	
 }
+
